a3f79706fba8306a459b9eaa234a022e1fbc0f0a
Chris <christopherbeagles@gmail.com>
2023-04-24 19:47:55 +0100

    Add more sample queries

--- sample_queries.py
+++ sample_queries.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python
 """A selection of sample queries on the seeded dataset"""
 
-from application import app, db, User
+from application import app, db, User, Cat
 
+# We have to use app.app_context() as the application isn't actually running in this file
 with app.app_context():
     # Select all users
     all_users = db.session.execute(db.select(User)).scalars().all()
@@ -46,3 +47,38 @@ with app.app_context():
     users_with_an_e = db.session.execute(db.select(User).where(User.name.like('%e'))).scalars().all()
     print(f'The following users name end with an e: {[user.name for user in users_with_an_e]}')
 
+    # Insert a new user
+    new_user = User()
+    new_user.name = "A new user"
+    db.session.add(new_user)
+    db.session.commit()
+
+    # Get the number of users with the name "A new user"
+    new_user_count = db.session.query(User).where(User.name == 'A new user').count()
+    print(f'We have {new_user_count} users with the name "A new user"')
+
+    # Insert a User and their Cats with models
+    cat_owner = User()
+    cat_owner.name = "Cat Owner"
+
+    new_cat = Cat()
+    new_cat.name = "New Cat"
+    cat_owner.cats.append(new_cat)
+
+    # Ensure we persist both the new user and the new cat!
+    db.session.add(new_user)
+    db.session.add(new_cat)
+    db.session.commit()
+    print(f'Cat Owner\'s cat is called: {cat_owner.cats[0].name}')
+
+    # Lets rename that cat
+    new_cat.name = 'Georgina'
+    db.session.commit()
+    print(f'Cat Owner\'s cat is now called: {cat_owner.cats[0].name}')
+
+    # Let's delete those new rows and keep things tidy
+    db.session.delete(new_cat)
+    db.session.delete(cat_owner)
+    db.session.delete(new_user)
+    db.session.commit()
+    print(f'All those items have now been deleted!')
